74810c202881ce9e98c0af863e5493e176e23f96
Jean-Michel Cazaux <jean-michel.cazaux@getalma.eu>
2023-11-05 14:29:53 +0100

    Added Kotlin version

---
+++ learning-spring-kotlin/src/main/kotlin/com/ironbird/learningspring/LearningSpringKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.ironbird.learningspring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LearningSpringKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<LearningSpringKotlinApplication>(*args)
+}
---
+++ learning-spring-kotlin/src/test/kotlin/com/ironbird/learningspring/LearningSpringKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ironbird.learningspring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LearningSpringKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
